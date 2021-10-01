6e47428aa55b21c47c10d34713feb9f6220844e2
Ylli <yllmemeti1@gmail.com>
2021-10-01 14:05:56 +0200

    initial commit

---
+++ ValidationHelperKosovo/src/main/java/com/ylli/validationhelperkosovo/NumberValidator.kt
@@ -0,0 +1,10 @@
+package com.ylli.validationhelperkosovo
+
+/**
+ * Created by Yll Memeti on 10/1/2021.
+ */
+class NumberValidator {
+    companion object{
+        fun a() :Int = 1
+    }
+}
---
+++ app/src/main/java/com/ylli/kosovovalidator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ylli.kosovovalidator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
